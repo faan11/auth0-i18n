d79b58d69bc9e8c4d8ca52e664daa2ebd1312fb2
Fabio <delphibit@gmail.com>
2021-12-01 23:29:53 +0100

    Remove old url in gentoken script

--- gentoken.sh
+++ gentoken.sh
@@ -2,7 +2,7 @@
 source .env
 #echo $CLIENT
 res=$(curl -s --request POST \
-  --url https://myhomeagenda.eu.auth0.com/oauth/token \
+  --url $AUD/oauth/token \
   --header 'content-type: application/json' \
   --data "{\"client_id\":\"$CLIENT\",\"client_secret\":\"$CS\",\"audience\":\"$AUD\",\"grant_type\":\"client_credentials\"}" | jq -r .access_token)
 echo "TOKEN=$res" > .at
